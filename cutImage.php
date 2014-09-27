88ead6cf25187037c2de44660d2815926c2c0f9d
mc-zone <mc@mc-zone.me>
2014-09-27 20:14:25 +0800

    修改裁剪图返回的URL，兼容多级目录站点

--- cutImage.php
+++ cutImage.php
@@ -8,6 +8,9 @@ $file = rtrim(dirname(__FILE__),'/').'/upload/'.$filename;
 $cutPicfolder = 'upload/cut/';
 $cutPicPath = rtrim(dirname(__FILE__),'/').'/'.$cutPicfolder;
 
+$urlPath = get_current_url();
+$urlPath = rtrim($urlPath,'/').'/';
+
 $x1 = $_POST['offsetLeft'];
 $y1 = $_POST['offsetTop'];
 $width = $_POST['width'];
@@ -57,11 +60,38 @@ if(!in_array($type, $support_type,true)) {
     @unlink($file);
 
     $data['status'] = 1;
-    $data['path'] =  $cutPicfolder.$newName;
-    $data['name'] =  $newName;
-    $data['url'] =  'http://'.rtrim($_SERVER['HTTP_HOST'],'/').'/'.$data['path'];
+    $data['path'] = $cutPicfolder.$newName;
+    $data['name'] = $newName;
+    $data['url']  = $urlPath.$data['path'];
 
     echo json_encode($data);
     exit;
 
+}
+
+function get_current_url($strip = true){
+    // filter function
+    $filter = function($input, $strip) {
+        $input = urldecode($input);
+        $input = str_ireplace(array("\0", '%00', "\x0a", '%0a', "\x1a", '%1a'), '', $input);
+        if ($strip) {
+            $input = strip_tags($input);
+        }
+        $input = htmlentities($input, ENT_QUOTES, 'UTF-8'); // or whatever encoding you use...
+        return trim($input);
+    };
+
+    $url = array();
+    // set protocol
+    $url['protocol'] = 'http://';
+    if (isset($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) === 'on' || $_SERVER['HTTPS'] == 1)) {
+        $url['protocol'] = 'https://';
+    } elseif (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443) {
+        $url['protocol'] = 'https://';
+    }
+    // set host
+    $url['host'] = $_SERVER['HTTP_HOST'];
+    // set request uri in a secure way
+    $url['request_uri'] = $filter( dirname($_SERVER['REQUEST_URI']), $strip);
+    return join('', $url);
 }
